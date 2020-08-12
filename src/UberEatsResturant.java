15089fe7cd2f8c7038098e933b6ccc591159abbf
Ontefetse-Ditsele <ontefetse.ditsele@outlook.com>
2020-08-12 18:15:46 +0200

    Created and Interface for Uber Eats Resturant & a Dish class.

---
+++ src/UberEatsResturant.java
@@ -0,0 +1,13 @@
+/**
+ * Ontefetse Ditsele
+ * 
+ */
+
+public interface UberEatsResturant {
+
+    //Methods that have to be Overriden by any Uber Eats Resturant.
+    public Dish[] getSignatureDishes();
+    public String getLocation();
+    public String getName(); 
+
+}
