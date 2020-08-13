a7beeed1e906317aab98b79e389f2b478a8844fd
Londolani <Londolani0700@gmail.com>
2020-08-13 07:25:08 +0200

    Edditted drop off and pick up for driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,8 @@
 public class Driver extends Person {
     Bike bike; 
     String licenseID;
+    String pickUp;
+    String dropOff;
     
     Driver(Bike bike, String licenseID, String name, String surname,String phone_number, int cash){
         super(name,surname,phone_number,cash);
@@ -16,5 +18,10 @@ public class Driver extends Person {
     public Car getBike(){return this.bike;}
 
     public String getlicenseID(){return  this.licenseID;}
+
+    public String setPickUp(String newPickUp){ this.pickUp = newPickUp;}
+
+    public String setDropOff(String newDropOff){ this.dropOff = newDropOff;}
+
     public String toString(){return super.toString() + " and I am your food delivery driver";}
 }
